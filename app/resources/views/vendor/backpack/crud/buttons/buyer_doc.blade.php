5c32162342c6aff090bd7b9557166cfed1682d62
developerAlternatecno <developer@alternatecno.es>
2023-05-09 11:18:54 +0200

    fix: fixed purchase list button to be disabled when the buy doesnt require doc

--- app/resources/views/vendor/backpack/crud/buttons/buyer_doc.blade.php
+++ app/resources/views/vendor/backpack/crud/buttons/buyer_doc.blade.php
@@ -1,1 +1,5 @@
-<a href="{{route('buyer-doc', $entry->id)}}" class="btn btn-sm btn-link"><i class="las la-download"></i>Buyer doc</a>
+@if($entry->buyer_doc)
+    <a href="{{route('buyer-doc', $entry->id)}}" class="btn btn-sm btn-link"><i class="las la-download"></i>Buyer doc</a>
+@else
+    <a href="{{route('buyer-doc', $entry->id)}}" class="btn btn-sm btn-link disabled"><i class="las la-download"></i>Buyer doc</a>
+@endif
