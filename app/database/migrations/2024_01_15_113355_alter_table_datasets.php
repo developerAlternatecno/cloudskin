b6c38fddf32d5999d43283f066ca17603d6f0946
developerAlternatecno <developer@alternatecno.es>
2024-01-15 12:29:00 +0100

    Alter table para las nuevas licencias

--- app/database/migrations/2024_01_15_113355_alter_table_datasets.php
+++ app/database/migrations/2024_01_15_113355_alter_table_datasets.php
@@ -10,7 +10,10 @@ class AlterTableDatasets extends Migration
     public function up()
     {
         // Actualizar el tipo de columna 'type' a ENUM
-        DB::statement("ALTER TABLE datasets MODIFY COLUMN type ENUM('sale', 'rental', 'free') DEFAULT 'sale'");
+        DB::statement("ALTER TABLE datasets MODIFY COLUMN type ENUM('sale', 'rental', 'free') DEFAULT NULL");
+
+        // Actualizar el tipo de columna 'license' a ENUM con nuevas opciones
+        DB::statement("ALTER TABLE datasets MODIFY COLUMN license ENUM('Unrestricted public use license', 'Public use license - include use restrictions', 'Public use license with geographic restrictions', 'Proprietary license - include usage restrictions', 'Proprietary license - include geographic restrictions') DEFAULT NULL");
 
         // Añadir la nueva columna booleana
         Schema::table('datasets', function (Blueprint $table) {
@@ -29,12 +32,20 @@ class AlterTableDatasets extends Migration
         // Revertir los registros existentes a 'buyout' en la columna "type"
         DB::table('datasets')->where('type', 'sale')->update(['type' => 'buyout']);
 
+        // Revertir los registros existentes a 'national' en la columna "license"
+        DB::statement("UPDATE datasets SET license = 'national' WHERE license = 'Unrestricted public use license'");
+        DB::statement("UPDATE datasets SET license = 'european' WHERE license = 'Public use license - include use restrictions'");
+        DB::statement("UPDATE datasets SET license = 'unlimited' WHERE license = 'Public use license with geographic restrictions'");
+
         // Eliminar la columna "autovalidate_sales"
         Schema::table('datasets', function (Blueprint $table) {
             $table->dropColumn('autovalidate_sales');
         });
 
         // Modificar la columna "type" para volver a sus valores originales
-        DB::statement("ALTER TABLE datasets MODIFY COLUMN type ENUM('buyout', 'rental') DEFAULT 'buyout'");
+        DB::statement("ALTER TABLE datasets MODIFY COLUMN type ENUM('buyout', 'rental') DEFAULT NULL");
+
+        // Modificar la columna "license" para volver a sus valores originales
+        DB::statement("ALTER TABLE datasets MODIFY COLUMN license ENUM('national', 'european', 'unlimited') DEFAULT NULL");
     }
 }
