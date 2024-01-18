708c601778e4145d2e055de4024e80f98fd3d25a
developerAlternatecno <developer@alternatecno.es>
2024-01-18 09:57:08 +0100

    Migrate Table User Fix: comentamos nif para ejecutar el migrate en PRO

--- app/database/migrations/2024_01_15_102800_alter_table_users.php
+++ app/database/migrations/2024_01_15_102800_alter_table_users.php
@@ -11,7 +11,7 @@ class AlterTableUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('password_confirmation', 255)->nullable()->after('password');
             $table->string('nationality', 30)->nullable()->after('password_confirmation');
-            $table->string('nif', 9)->nullable()->after('nationality');
+            //$table->string('nif', 9)->nullable()->after('nationality');
             $table->string('documento_identidad', 250)->nullable()->after('nif');
             
         });
@@ -26,7 +26,7 @@ class AlterTableUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('nationality');
-            $table->dropColumn('nif');
+            ///$table->dropColumn('nif');
             $table->dropColumn('documento_identidad');
         });
     }
