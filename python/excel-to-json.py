e8a2b76283f3bd6a69a1a4cfbc14da88f49225c1
developerAlternatecno <developer@alternatecno.es>
2024-01-20 00:47:18 +0100

    Modificaion PY

--- python/excel-to-json.py
+++ python/excel-to-json.py
@@ -11,11 +11,9 @@ from typing import Optional
 def upload_data(excel_file_path, url, latitude: Optional[float]=None, longitude: Optional[float]=None):
     print('Entro a upload_data')
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
 
     if excel_file_path:
-        excel_file_path = os.path.join(script_dir,"app/storage/app/", excel_file_path)
-        excel_file_path = excel_file_path.replace("python/", "")
+        excel_file_path = os.path.join("CloudSkin/cloudksin/app/storage/app/", excel_file_path)
     
     # logging.basicConfig(level=logging.INFO)
     # logger = logging.getLogger(__name__)
