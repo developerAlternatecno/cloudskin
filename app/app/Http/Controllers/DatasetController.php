ac7f5443ba58f3c05d646cd01f6c8781f0a3cdcf
developerAlternatecno <developer@alternatecno.es>
2024-01-21 19:07:23 +0100

    Debug create Dataread

--- app/app/Http/Controllers/DatasetController.php
+++ app/app/Http/Controllers/DatasetController.php
@@ -65,7 +65,7 @@ class DatasetController extends Controller
             if ($positionOfCelsius === false) {
                 $positionOfCelsius = array_search('°C', $set2);
                 Log::info("Debemos reemplazar '°C' por 'ºC'");
-                $set2[3] = 'ºC';
+                $set[$positionOfCelsius] = 'ºC';
             }
 
             # Reemplazamos 'ºC' o '°C' con 'ºC' si se encuentra
