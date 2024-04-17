4af83b10f8d34db777fd824d57d45e0264cc166f
developerAlternatecno <developer@alternatecno.es>
2024-04-17 12:30:50 +0200

    Fix URL map api

--- app/public/js/map.js
+++ app/public/js/map.js
@@ -5,7 +5,7 @@ function onMarkerClick(event, route){
 
 async function map_admin_pins(url){
 
-    let response = await fetch(url+'/api/map/datarsets',{
+    let response = await fetch(url+'/api/map/datasets',{
         method: "GET"
     })
 
