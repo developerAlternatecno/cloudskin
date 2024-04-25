82efc95c6ad74adc76e747e87bb751c65d0b8eda
raulcarriscajo <raulcarriscajo@biyectiva.com>
2024-04-25 17:30:45 +0200

    imagen

--- app/app/Http/Controllers/Admin/Dataset/MyDatasetCrudController.php
+++ app/app/Http/Controllers/Admin/Dataset/MyDatasetCrudController.php
@@ -101,7 +101,6 @@ class MyDatasetCrudController extends CrudController
         CRUD::column('description');
         CRUD::column('url');
         CRUD::column('type');
-        CRUD::column('imagen');
 
 
         // Agregar la tabla a la configuración de la vista de detalle
