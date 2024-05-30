04b4b1b5fe2f830c2e2cd61007eb7109bbadc2ff
raulcarriscajo <raulcarriscajo@biyectiva.com>
2024-05-30 14:15:06 +0200

    quiar reglado dataset vacio

--- app/app/Http/Controllers/Admin/Dataset/MyDatasetCrudController.php
+++ app/app/Http/Controllers/Admin/Dataset/MyDatasetCrudController.php
@@ -107,24 +107,24 @@ class MyDatasetCrudController extends CrudController
         $datasetId = $this->crud->getCurrentEntry()->id;
 
 
-        if ($datasetData === "<p>No data available</p>") {
-            $this->crud->addColumns([
-                [
-                    'name' => 'table_html',
-                    'label' => 'Last 5 records',
-                    'type' => 'custom_html',
-                    'value' => $datasetData . "<a href='" . route('dataset.upload_form', ['dataset' => $datasetId]) . "' class='btn btn-primary'>Upload data</a>",
-                ]
-            ]);
-        } else {
-            $this->crud->addColumns([
-                [
-                    'name' => 'table_html',
-                    'label' => 'Last 5 records',
-                    'type' => 'custom_html',
-                    'value' => Dataset::where('id', $this->crud->getCurrentEntry()->id)->first()->generateLastDataReadsTable(),
-                ]
-            ]);
-        }
+        // if ($datasetData === "<p>No data available</p>") {
+        $this->crud->addColumns([
+            [
+                'name' => 'table_html',
+                'label' => 'Last 5 records',
+                'type' => 'custom_html',
+                'value' => $datasetData . "<a href='" . route('dataset.upload_form', ['dataset' => $datasetId]) . "' class='btn btn-primary'>Upload data</a>",
+            ]
+        ]);
+        // } else {
+        //     $this->crud->addColumns([
+        //         [
+        //             'name' => 'table_html',
+        //             'label' => 'Last 5 records',
+        //             'type' => 'custom_html',
+        //             'value' => Dataset::where('id', $this->crud->getCurrentEntry()->id)->first()->generateLastDataReadsTable(),
+        //         ]
+        //     ]);
+        // }
     }
 }
