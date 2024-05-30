c0f14027b50742c228bd91179fc39cecc5a0d19e
raulcarriscajo <raulcarriscajo@biyectiva.com>
2024-05-30 09:20:20 +0200

    URL para upload JSON

--- app/routes/web.php
+++ app/routes/web.php
@@ -23,5 +23,6 @@ Route::get('/', function () {
 Route::get('/dataset/create/{projectId}', 
     [App\Http\Controllers\DatasetController::class, 'create'])->name('DatasetController@create')->name('dataset.create');
   
-Route::get('/upload/{dataset}', [App\Http\Controllers\DatasetController::class, 'showUploadForm'])->name('dataset.upload_form');
+Route::get('admin/upload/{dataset}', 
+    [App\Http\Controllers\DatasetController::class, 'showUploadForm'])->name('dataset.upload_form');
 Route::post('/upload/{dataset}', [App\Http\Controllers\DatasetController::class, 'upload'])->name('dataset.upload');
