5370e6a172ba034b85ed1cbcba023dc0585bc354
Antonio Manzano Muñoz <amanzanom98@gmail.com>
2025-02-12 16:28:39 +0100

    fix: fixed endpoint

--- app/public/js/map.js
+++ app/public/js/map.js
@@ -4,7 +4,7 @@ function onMarkerClick(event, route){
 }
 
 async function map_admin_pins(url){
-    let response = await fetch(url+'/api/map/datarsets',{
+    let response = await fetch(url+'/api/map/datasets',{
         method: "GET"
     })
 
